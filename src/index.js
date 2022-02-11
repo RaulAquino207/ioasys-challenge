69c1ca5b2b72e1b88b5012935776cb076cf30ab2
Raul Aquino <aquinoraul207@gmail.com>
2022-02-10 23:18:04 -0300

    added the file give routes and the swagger configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('swagger-json');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.listen(3000, () => {
-    console.log(`🚀 ~ Server started on port ~ ${3000}`);
+app.listen(port, () => {
+    console.log(`🚀 ~ Server started on port ~ ${port}`);
 })
