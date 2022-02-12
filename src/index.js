1083e6bccc0629f13c62994217243a582f189889
Raul Aquino <aquinoraul207@gmail.com>
2022-02-12 12:39:09 -0300

    creation of a small route to make the correct configuration of the swagger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('swagger-json');
+const swaggerDocs = require('./configs/swagger.json');
+const dotenv = require('dotenv');
+
+dotenv.config({path : 'src/.env'});
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
